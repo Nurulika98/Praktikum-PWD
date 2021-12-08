a4c52481decf99aa3d2b92ac9a6aff8c8f951585
Nurulika98 <48236921+Nurulika98@users.noreply.github.com>
2021-12-08 22:39:14 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,6 @@
-<?php
-
-$host = "localhost";
-$username = "root";
-$password = "";
-$databasename = "akademik";
-$connect = mysqli_connect($host, $username, $password, $databasename);
-if (!$con){
-	echo "Error: " . mysql_connect_error();
-	exit();
-}
+<?php 
+	$con = new mysqli("localhost", "root", "", "akademik");
+	if (!$con) {
+		echo "Gagal";	
+	}
+?>
